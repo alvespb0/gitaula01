00ae38f150149c96c1dfdb29bf34faf99eece3d8
Arthur Alves <IFSC@CDR-691785.cacador.ifsc.edu.br>
2021-11-16 15:32:20 -0300

    impressão com scanner

---
+++ src/githubarthuralves/GithubArthurAlves.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubarthuralves;
+import java.util.Scanner;
+
+public class GithubArthurAlves {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner (System.in);
+        String nome;
+        System.out.println("digite seu nome");
+        nome = leia.next();
+        System.out.println(nome+" Bem vindo ao GitHub");
+    }
+    
+}
